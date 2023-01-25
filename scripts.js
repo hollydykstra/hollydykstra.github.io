6713cbc7c504f5a6a70ff00b91988c47660d13e4
Tweedy Flanigan <tweedy@gmail.com>
2023-01-25 14:19:52 +0100

    asciimac

--- scripts.js
+++ scripts.js
@@ -13,11 +13,38 @@ function steam() {
 function blink() {
 	var elements = document.getElementsByClassName('blink');
 	for (var i = 0; i < elements.length; i = i+1) {
-		if(elements[i].innerHTML == '•') {elements[i].innerHTML = '<FONT COLOR="lightgreen">•</FONT>';}
-		else if(elements[i].innerHTML.toLowerCase() == '<font color="lightgreen">•</font>') {elements[i].innerHTML = '•';}
+		if(elements[i].innerHTML == ' • ') {elements[i].innerHTML = '<FONT COLOR="lightgreen"> • </FONT>';}
+		else if(elements[i].innerHTML.toLowerCase() == '<font color="lightgreen"> • </font>') {elements[i].innerHTML = ' • ';}
 	}
 }
+function clearScreen(message = "World!") {
+	if(document.getElementById('message').innerHTML == message) {
+	var intervalId = setInterval(function() {
+		for(var i = message.length - 1; i >= 0; i = i - 1) {
+			var display = document.getElementById('message').innerHTML;
+			if(display.charAt(i) == ' ') {} else {
+			display = display.substring(0, i) + ' ' + display.substring(i+1, message.length);
+			document.getElementById('message').innerHTML = display;
+			return;}
+		}
+		if(document.getElementById('message').innerHTML = '      ') {
+			if(document.getElementById('period').innerHTML == ',') {
+				document.getElementById('period').innerHTML = ' ';
+			}
+			else if(document.getElementById('period').innerHTML == ' ') {
+				document.getElementById('period').innerHTML = '.';
+				clearInterval(intervalId);
+			}
+		}
+		return;
+	}, 200);
+	}
+//	document.getElementById('message').innerHTML = '      ';
+//	document.getElementById('period').innerHTML = '.';
+}
 function type(message = "World!") {
+	if(!isScreenOn()){toggleScreen(); return;}
+	if(document.getElementById('message').innerHTML == message) { clearScreen(message); return;}
 	var intervalId = setInterval(function() {
 	if(document.getElementById('period').innerHTML == '.') {
 		document.getElementById('period').innerHTML = ' ';
@@ -41,20 +68,29 @@ function type(message = "World!") {
 	}
 	}}, 400);
 }
-function clearScreen() {
-	document.getElementById('message').innerHTML = '      ';
-	document.getElementById('period').innerHTML = '.';
-}
 function toggleScreen() {
 	var elements = document.getElementsByClassName('screen');
-	for(var i = 0; i < elements.length; i = i + 1) {
-		if(elements[i].style.backgroundColor == 'darkslategray') {
-			elements[i].style.backgroundColor = 'initial';
-			elements[i].style.color = 'initial';
+	if(isScreenOn() == false) {
+		for(var i = 0; i < elements.length; i = i + 1) {
+			elements[i].style.backgroundColor = 'white';
+			elements[i].style.color = 'black';
 		}
-		else {
+		blink();
+	}
+	else {
+		for(var i = 0; i < elements.length; i = i + 1) {
 			elements[i].style.backgroundColor = 'darkslategray';
 			elements[i].style.color = 'darkslategray';
 		}
+		blink();
+	}
+}
+function isScreenOn() {
+	var elements = document.getElementsByClassName('screen');
+	for(var i = 0; i < elements.length; i = i + 1) {
+		if(elements[i].style.backgroundColor == 'white') {
+			return true;
+		}
+		else {return false;}
 	}
 }
